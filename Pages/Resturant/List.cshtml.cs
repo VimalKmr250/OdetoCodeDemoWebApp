bfad848af0c476c82e02d267810ee5f645ef5e41
Vimalkumar <Vimalkmr250@gmail.com>
2022-07-17 00:51:56 +0530

    Module 2 Inmemory data

--- Pages/Resturant/List.cshtml.cs
+++ Pages/Resturant/List.cshtml.cs
@@ -4,13 +4,31 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using OdetoCode.Data;
+using OdeToCode.Food;
 
 namespace OdetoCodeDemoWebApp.Pages.Resturant
 {
     public class ListModel : PageModel
     {
+        private readonly IConfiguration configuration;
+        private readonly IResturantData resturantData;
+
+        public ListModel(IConfiguration configuration, IResturantData resturantData)
+        {
+            this.configuration = configuration;
+            this.resturantData = resturantData;
+        }
+
+        public string Message { get; set; }
+        public IEnumerable<Resturants> resturants{ get; set; }
         public void OnGet()
         {
+            Message = configuration["Message"];
+
+            resturants = resturantData.GetAll();
+
         }
     }
 }
